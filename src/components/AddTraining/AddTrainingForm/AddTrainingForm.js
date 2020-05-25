ea0dac0a02d0d18239158c8a43eb0f93d6786ec2
kj-ninja <jakubiak.krzy@gmail.com>
2020-05-25 15:03:16 +0200

    add forik to stepTwo form in addtraining comp

--- src/components/AddTraining/AddTrainingForm/AddTrainingForm.js
+++ src/components/AddTraining/AddTrainingForm/AddTrainingForm.js
@@ -1,72 +1,25 @@
 import React, {useState} from 'react';
-import {Form, Formik} from "formik";
-import * as Yup from "yup";
-import Button from "react-bootstrap/Button";
 import AddTrainingFormStepTwo from "./AddTrainingFormStepTwo";
 import AddTrainingFormStepOne from './AddTrainingFormStepOne';
 
-const formInputs = {
-    date: new Date().toISOString().substr(0, 10), name: '', duration: '', kcal: '', notes: '', selectedRepetitions: '',
-    selectedExerciseTime: '', selectedWeight: ''
-};
-const validationSchema = Yup.object({
-    name: Yup.string()
-        .max(50, 'Maksymalnie 50 znaków')
-        .required('Pole wymagane'),
-    duration: Yup.string()
-        .required('Pole wymagane'),
-    selectedRepetitions: Yup.string()
-        .matches(/^[0-9]+[0-9]*$/, 'Podana wartość musi być liczbą'),
-    selectedExerciseTime: Yup.string()
-        .matches(/^[0-9]+[0-9]*$/, 'Podana wartość musi być liczbą'),
-    selectedWeight: Yup.string()
-        .matches(/^[0-9]+[0-9]*$/, 'Podana wartość musi być liczbą')
-});
-
-const AddTrainingForm = ({setSelectedExercise, handleAddTraining, handleAddSet}) => {
+const AddTrainingForm = ({setSelectedExercise, handleAddTraining, handleAddSet, selectedExercise}) => {
     const [stepOne, setStepOne] = useState(true);
     const [stepTwo, setStepTwo] = useState(false);
+    const [valuesStepOne, setValuesStepOne] = useState();
 
-    // @TODO
-    // zrobic togglera !prevState i dodac button wstecz
-    const handleStep = () => {
+    const handleNextStep = () => {
         setStepOne(false);
         setStepTwo(true);
     };
 
     return (
-        <Formik
-            initialValues={formInputs}
-            validationSchema={validationSchema}
-            onSubmit={(values) => {
-                console.log(values);
-                handleAddTraining(values);
-            }}
-        >
-            {({values}) => {
-                return (
-                    <Form className="add-training__form">
-                        <div className="add-training__container">
-
-                            {stepOne ? <AddTrainingFormStepOne values={values}/> : null}
-                            {stepTwo ? <AddTrainingFormStepTwo setSelectedExercise={setSelectedExercise}/> : null}
-
-                            <div className="add-training__buttons-container">
-                                {stepOne ? <Button className="btn btn-primary align-self-end" onClick={handleStep}>Dalej</Button> :
-                                    <>
-                                        <Button className="btn btn-primary" onClick={() => handleAddSet(values)}>Dodaj
-                                            serię
-                                        </Button>
-                                        <Button type="submit" className="btn btn-success pull-right">Zapisz trening
-                                        </Button>
-                                    </>
-                                }
-                            </div>
-                        </div>
-                    </Form>
-                );
-            }}
-        </Formik>
+        <>
+            {stepOne ? <AddTrainingFormStepOne handleNextStep={handleNextStep} setValuesStepOne={setValuesStepOne}/> : null}
+            {stepTwo ? <AddTrainingFormStepTwo setSelectedExercise={setSelectedExercise}
+                                               handleAddTraining={handleAddTraining}
+                                               handleAddSet={handleAddSet}
+                                               valuesStepOne={valuesStepOne}/> : null}
+        </>
     );
 };
 
--- src/components/AddTraining/AddTrainingForm/AddTrainingFormStepOne.js
+++ src/components/AddTraining/AddTrainingForm/AddTrainingFormStepOne.js
@@ -1,50 +1,74 @@
 import React from 'react';
-import {ErrorMessage, Field} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import * as Yup from "yup";
+import Button from "react-bootstrap/Button";
 
-const AddTrainingFormStepOne = ({values}) => {
+const AddTrainingFormStepOne = ({handleNextStep, setValuesStepOne}) => {
 
-    function validateUsername(values) {
-        let error;
-        if (values.name === 'admin') {
-            error = 'Nice try!';
-        }
-        return error;
-    }
+
+    const formInputs = {
+        date: new Date().toISOString().substr(0, 10), name: '', duration: '', kcal: '', notes: ''
+    };
+    const validationSchema = Yup.object({
+        name: Yup.string()
+            .max(50, 'Maksymalnie 50 znaków')
+            .required('Pole wymagane'),
+        duration: Yup.string()
+            .required('Pole wymagane')
+    });
 
     return (
-        <>
-            <div className="add-training__form-group">
-                <label htmlFor="date">Data:</label>
-                <Field name="date" type="date" className="form-control form-control-sm"/>
-            </div>
-            <div className="add-training__form-group">
-                <label htmlFor="name">Nazwa:</label>
-                <Field name="name" type="text" placeholder="Podaj nazwę treningu"
-                       className="form-control form-control-sm" validate={validateUsername(values)}/>
-                {}
-                <p className="add-training__error-message">
-                    <ErrorMessage name="name"/>
-                </p>
-            </div>
-            <div className="add-training__form-group">
-                <label htmlFor="duration">Czas trwania:</label>
-                <Field name="duration" type="text" placeholder="Podaj czas trwania treningu"
-                       className="form-control form-control-sm"/>
-                <p className="add-training__error-message">
-                    <ErrorMessage name="duration"/>
-                </p>
-            </div>
-            <div className="add-training__form-group">
-                <label htmlFor="kcal">Kcal:</label>
-                <Field name="kcal" type="text" placeholder="Ilość spalonych kalorii"
-                       className="form-control form-control-sm"/>
-                <ErrorMessage name="kcal"/>
-            </div>
-            <div className="add-training__form-group notes">
-                <label htmlFor="notes">Notatki:</label>
-                <Field name="notes" as="textarea" className="form-control" rows={3}/>
-            </div>
-        </>
+            <Formik
+                initialValues={formInputs}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
+                    setValuesStepOne(values);
+                    handleNextStep(values);
+                }}
+            >
+                {() => {
+                    return (
+                        <Form className="add-training__form">
+                            <div className="add-training__container">
+                                <div className="add-training__form-group">
+                                    <label htmlFor="date">Data:</label>
+                                    <Field name="date" type="date" className="form-control form-control-sm"/>
+                                </div>
+                                <div className="add-training__form-group">
+                                    <label htmlFor="name">Nazwa:</label>
+                                    <Field name="name" type="text" placeholder="Podaj nazwę treningu"
+                                           className="form-control form-control-sm"/>
+                                    <p className="add-training__error-message">
+                                        <ErrorMessage name="name"/>
+                                    </p>
+                                </div>
+                                <div className="add-training__form-group">
+                                    <label htmlFor="duration">Czas trwania:</label>
+                                    <Field name="duration" type="text" placeholder="Podaj czas trwania treningu"
+                                           className="form-control form-control-sm"/>
+                                    <p className="add-training__error-message">
+                                        <ErrorMessage name="duration"/>
+                                    </p>
+                                </div>
+                                <div className="add-training__form-group">
+                                    <label htmlFor="kcal">Kcal:</label>
+                                    <Field name="kcal" type="text" placeholder="Ilość spalonych kalorii"
+                                           className="form-control form-control-sm"/>
+                                    <ErrorMessage name="kcal"/>
+                                </div>
+                                <div className="add-training__form-group notes">
+                                    <label htmlFor="notes">Notatki:</label>
+                                    <Field name="notes" as="textarea" className="form-control" rows={3}/>
+                                </div>
+
+                                <div className="add-training__buttons-container">
+                                    <Button className="btn btn-primary align-self-end" type="submit">Dalej</Button>
+                                </div>
+                            </div>
+                        </Form>
+                    );
+                }}
+            </Formik>
     );
 };
 
