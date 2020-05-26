8791486b7ebe907b70cdb48a2f1114e8b6b28862
kj-ninja <jakubiak.krzy@gmail.com>
2020-05-26 14:14:21 +0200

    big-six refactor

--- src/components/BigSix/BigSix.js
+++ src/components/BigSix/BigSix.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import './BigSix.scss';
+import {Link} from "react-router-dom";
 import useWindowWidth from '../../functions/hooks/useWindowWidth';
-import BigSixMobile from "./BigSixMobile/BigSixMobile";
-import BigSixDesktop from "./BigSixDesktop/BigSixDesktop";
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import Button from "react-bootstrap/Button";
+import handleLogout from "../../functions/logout";
+import Header from "../Header/Header";
+import BigSixTable from "./BigSixTable/BigSixTable";
 
 const BigSix = () => {
     const width = useWindowWidth();
 
-    if (width < 650) {
-        return <BigSixMobile/>
-    }
-    return <BigSixDesktop/>
+    return (
+        <>
+            {width < 650 ? <HamburgerMenu/> :
+            <Header logoLink={"/main"}>
+                    <div className="big-six__buttons--desktop">
+                        <Link to="/big-six"><Button variant="primary">Wielka szóstka</Button></Link>
+                        <Link to="/add-training"><Button variant="primary">Dodaj trening</Button></Link>
+                        <Link to="/"><Button className="big-six__btn--logout" onClick={handleLogout}
+                                             variant="secondary">
+                            Wyloguj się</Button>
+                        </Link>
+                    </div>
+                </Header>}
+
+            <BigSixTable/>
+        </>
+    )
 };
 
 export default BigSix;
