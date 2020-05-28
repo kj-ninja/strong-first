6b22c481061d24a94966fef1d76484bf19b8e1cb
kj-ninja <jakubiak.krzy@gmail.com>
2020-05-28 13:15:19 +0200

    commit before new branch, transitions

--- src/components/AddTraining/AddTrainingForm/AddTrainingFormStepOne.js
+++ src/components/AddTraining/AddTrainingForm/AddTrainingFormStepOne.js
@@ -4,8 +4,6 @@ import * as Yup from "yup";
 import Button from "react-bootstrap/Button";
 
 const AddTrainingFormStepOne = ({handleNextStep, setValuesStepOne}) => {
-
-
     const formInputs = {
         date: new Date().toISOString().substr(0, 10), name: '', duration: '', kcal: '', notes: ''
     };
@@ -18,57 +16,57 @@ const AddTrainingFormStepOne = ({handleNextStep, setValuesStepOne}) => {
     });
 
     return (
-            <Formik
-                initialValues={formInputs}
-                validationSchema={validationSchema}
-                onSubmit={(values) => {
-                    setValuesStepOne(values);
-                    handleNextStep(values);
-                }}
-            >
-                {() => {
-                    return (
-                        <Form className="add-training__form">
-                            <div className="add-training__container">
-                                <div className="add-training__form-group">
-                                    <label htmlFor="date">Data:</label>
-                                    <Field name="date" type="date" className="form-control form-control-sm"/>
-                                </div>
-                                <div className="add-training__form-group">
-                                    <label htmlFor="name">Nazwa:</label>
-                                    <Field name="name" type="text" placeholder="Podaj nazwę treningu"
-                                           className="form-control form-control-sm"/>
-                                    <p className="add-training__error-message">
-                                        <ErrorMessage name="name"/>
-                                    </p>
-                                </div>
-                                <div className="add-training__form-group">
-                                    <label htmlFor="duration">Czas trwania:</label>
-                                    <Field name="duration" type="text" placeholder="Podaj czas trwania treningu"
-                                           className="form-control form-control-sm"/>
-                                    <p className="add-training__error-message">
-                                        <ErrorMessage name="duration"/>
-                                    </p>
-                                </div>
-                                <div className="add-training__form-group">
-                                    <label htmlFor="kcal">Kcal:</label>
-                                    <Field name="kcal" type="text" placeholder="Ilość spalonych kalorii"
-                                           className="form-control form-control-sm"/>
-                                    <ErrorMessage name="kcal"/>
-                                </div>
-                                <div className="add-training__form-group notes">
-                                    <label htmlFor="notes">Notatki:</label>
-                                    <Field name="notes" as="textarea" className="form-control" rows={3}/>
-                                </div>
-
-                                <div className="add-training__buttons-container">
-                                    <Button className="btn btn-primary align-self-end" type="submit">Dalej</Button>
-                                </div>
+        <Formik
+            initialValues={formInputs}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+                setValuesStepOne(values);
+                handleNextStep(values);
+            }}
+        >
+            {() => {
+                return (
+                    <Form className="add-training__form">
+                        <div className="add-training__container">
+                            <div className="add-training__form-group">
+                                <label htmlFor="date">Data:</label>
+                                <Field name="date" type="date" className="form-control form-control-sm"/>
                             </div>
-                        </Form>
-                    );
-                }}
-            </Formik>
+                            <div className="add-training__form-group">
+                                <label htmlFor="name">Nazwa:</label>
+                                <Field name="name" type="text" placeholder="Podaj nazwę treningu"
+                                       className="form-control form-control-sm"/>
+                                <p className="add-training__error-message">
+                                    <ErrorMessage name="name"/>
+                                </p>
+                            </div>
+                            <div className="add-training__form-group">
+                                <label htmlFor="duration">Czas trwania:</label>
+                                <Field name="duration" type="text" placeholder="Podaj czas trwania treningu"
+                                       className="form-control form-control-sm"/>
+                                <p className="add-training__error-message">
+                                    <ErrorMessage name="duration"/>
+                                </p>
+                            </div>
+                            <div className="add-training__form-group">
+                                <label htmlFor="kcal">Kcal:</label>
+                                <Field name="kcal" type="text" placeholder="Ilość spalonych kalorii"
+                                       className="form-control form-control-sm"/>
+                                <ErrorMessage name="kcal"/>
+                            </div>
+                            <div className="add-training__form-group notes">
+                                <label htmlFor="notes">Notatki:</label>
+                                <Field name="notes" as="textarea" className="form-control" rows={3}/>
+                            </div>
+
+                            <div className="add-training__buttons-container">
+                                <Button className="btn btn-primary align-self-end" type="submit">Dalej</Button>
+                            </div>
+                        </div>
+                    </Form>
+                );
+            }}
+        </Formik>
     );
 };
 
