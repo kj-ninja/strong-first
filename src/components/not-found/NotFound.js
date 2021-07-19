157c90b1ccfde0dcd09601fc02b0c1dcb1057ee1
mojzesz13 <bozejewski@gmail.com>
2021-07-19 22:18:03 +0200

    app corected

--- src/components/not-found/NotFound.js
+++ src/components/not-found/NotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+import { Result, Button } from 'antd';
+import './NotFound.scss';
+import {Link} from "react-router-dom";
 
-const NotFound = () => {
+const NotFound = ({title, text, status}) => {
     return (
-        <h1>
-           404, Nothing is here
-        </h1>
+      <Result
+        status={status ? status : "404"}
+        title={title ? title : "error 404"}
+        subTitle={text ? text : "Coś tu poszło nie tak jak planowaliśmy Mistrzu"}
+        extra={<Link to={"/"}><Button type="primary">Strona główna</Button></Link>}
+        className="errorContainer"
+      />
     );
 };
 
