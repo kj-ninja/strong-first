4b6ae43e4dde2ac7cde6182c3c88c20d60f9f943
mojzesz13 <bozejewski@gmail.com>
2021-07-20 21:57:46 +0200

    added  Spinner

--- src/components/ui/spinner/Spinner.js
+++ src/components/ui/spinner/Spinner.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import './Spinner.scss';
+import 'antd/dist/antd.css';
+import { LoadingOutlined } from '@ant-design/icons';
+import "./Spinner.scss"
 
-const Spinner = (props) => <div className="loader" style={props.dimensions}>Loading...</div>
+
+const Spinner = () =>{
+  return (
+    <LoadingOutlined className="spinnerContainer" spin />
+  )
+
+}
 
 export default Spinner;
