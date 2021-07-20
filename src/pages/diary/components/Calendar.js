ff41ec70a232127cab61afd71274787e6ff3124d
kj-ninja <jakubiak.krzy@gmail.com>
2021-07-20 15:01:56 +0200

    refactored trainings summary component also added new state data to calendar like picked trainings

--- src/pages/diary/components/Calendar.js
+++ src/pages/diary/components/Calendar.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {setPickedDate} from "../../../store/actions/calendar.actions";
+import {setPickedTrainings} from "../../../store/actions/calendar-trainings.actions";
 import {getDay, cutWeekDay, isToday, isNormalDay, getMonthData} from "../helpers";
 import bindClassesDynamically from "../../../utils/classBinder";
 import './Calendar.scss';
 
 const Calendar = (props) => {
-  const {setPickedDate, pickedDate, daysOfWeek, calendarStructure, pickedMonth} = props;
+  const {setPickedDate, pickedDate, daysOfWeek, calendarStructure, pickedMonth, setPickedTrainings} = props;
 
   const handleDatePick = (day, dayIndex) => {
-    console.log('Yo kliknales w dzien: ', day);
     const isDatePicked = pickedDate === day.date;
     if (!isDatePicked && !day.isDiffMonth) {
       setPickedDate({date: day.date, dayIndex});
+    }
+
+    if (day.trainings.length) {
+      console.log('Yo kliknales w dzien z treningami');
+      console.log('Dzien: ', day);
+      console.log('Treningi: ', day.trainings);
+      setPickedTrainings(day.trainings);
+    } else {
+      setPickedTrainings([]);
     }
   };
 
@@ -64,4 +73,4 @@ const mapStateToProps = state => ({
   daysOfWeek: state.calendar.daysOfWeek,
 });
 
-export default connect(mapStateToProps, {setPickedDate})(Calendar);
+export default connect(mapStateToProps, {setPickedDate, setPickedTrainings})(Calendar);
